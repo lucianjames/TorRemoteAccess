f013f93c86bc1d1174b1fa89a4a5b8eefe476df4
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-12-26 18:49:58 +0000

    Update yourmum.py

--- yourmum.py
+++ yourmum.py
@@ -1,24 +1,11 @@
 import socket
 import random
+from time import sleep
 
-# Send the following string to the server:
-# <ip>;<username>;<hostname>;
-
-# Fill each out with something appropriate
-
-
-socks = []
-for i in range (0, 1000):
-    socks.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-    socks[i].connect(("127.0.0.1", 8080))
-    print("Connected to server {}".format(i))
-    socks[i].send("127.0.0.1;{};{};".format("sussyballs", socket.gethostname()).encode("utf-8"))
-    print("Sent data to server {}".format(i))
-    d = socks[i].recv(1024)
-    print("Received data from server {}: {}".format(i, d))
-
-    
-
-# Close all the connections
-for s in socks:
-    s.close()
+while(1):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("127.0.0.1", 8080))
+    sock.send("127.0.0.1;{};{};".format("sussyballs", socket.gethostname()).encode("utf-8"))
+    data = sock.recv(1024)
+    sock.send(data)
+    sock.close()
